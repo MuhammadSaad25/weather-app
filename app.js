ce44d8c57310694c02d63b9ae116aeed2ba9d080
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-07-01 14:42:10 +0500

    icon uncomment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let myFunction = () =>{
        
        let icon = data.current.condition.icon;
        icon.replace("/file// ");
-    // document.getElementById("weather_icon").src = icon;
+    document.getElementById("weather_icon").src = icon;
     document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
     // document.querySelector("#tempF").innerText = data.current.temp_f + "°F";
 
