a37d8cef6aab14f5769d3f7df32f2810cbdc5fab
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-07-03 04:46:43 +0500

    complate responsive Header

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ let myFunction = () =>{
        icon.replace("/file// ");
     document.getElementById("weather_icon").src = icon;
     document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
-    // document.querySelector("#tempF").innerText = data.current.temp_f + "°F";
+    
+    document.querySelector("#tempF").innerText = data.current.temp_f + "°F";
 
 
     document.querySelector("#city_name").innerText = data.location.name + ", " ;
+    document.querySelector("#time").innerText = data.location.localtime + ", " ;
+    document.querySelector("#last_update").innerText = data.current.last_updated + ", " ;
     document.querySelector("#weather_condition").innerText = data.current.condition.text;
 
     document.querySelector("#feels_like").innerText = "Feels Like: "+ data.current.feelslike_c + "°C";
