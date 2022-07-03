56d8cf4d8071d57e2c6a2c4f71dc18e8d3e4587e
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-07-03 16:12:03 +0500

    half work done

--- app.js
+++ app.js
@@ -23,11 +23,13 @@ let myFunction = () =>{
 
     document.querySelector("#city_name").innerText = data.location.name + ", " ;
     document.querySelector("#time").innerText = data.location.localtime + ", " ;
-    document.querySelector("#last_update").innerText = data.current.last_updated + ", " ;
+    document.querySelector("#last_update").innerText = data.current.last_updated + "   Last Update" ;
     document.querySelector("#weather_condition").innerText = data.current.condition.text;
 
-    document.querySelector("#feels_like").innerText = "Feels Like: "+ data.current.feelslike_c + "°C";
-    document.querySelector("#wind_speed").innerText = "Wind Speed: " + data.current.wind_kph + "KP/H";
+    document.querySelector("#feels_like_c").innerText = "Feels Like: "+ data.current.feelslike_c + "°C";
+    document.querySelector("#feels_like_f").innerText = "Feels Like: "+ data.current.feelslike_f + "°F";
+    document.querySelector("#wind_speed_kph").innerText = "Wind Speed: " + data.current.wind_kph + "KP/H";
+    document.querySelector("#wind_speed_mph").innerText = "Wind Speed: " + data.current.wind_mph + "MP/H";
     document.querySelector("#humidity").innerText = "Humidity: " + data.current.humidity + "%";
 
 
@@ -35,7 +37,8 @@ let myFunction = () =>{
     
 
 
-    // getWeatherData();  
+    // getWeatherData();
+    // myFunction ();  
 
     })
 
